e20384975347efe1f4a6dabc9040c2c7adb248ec
何育昆 <heyukun@heyukundeMacBook-Pro.local>
2017-04-12 17:46:37 +0800

    swift

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var yearEdit: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = -9;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+   
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        yearEdit.resignFirstResponder();
+    }
+    
+    @IBAction func okTapped(_ sender: Any) {
+        if let year = Int(yearEdit.text!){
+            
+            let imageNumber = (year - offset)%12
+            image.image = UIImage(named: String(imageNumber));
+        }
+        else{
+            
+        };
     }
 
     override func didReceiveMemoryWarning() {
